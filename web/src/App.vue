a4db8e98e651f8571bdc48a233ae3e1f26bed613
yangzccc666 <yangzccc@gmail.com>
2023-01-06 21:25:18 +0800

    后端实现用户登录和加密验证

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,57 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import $ from 'jquery'
 
 export default {
   components:{
     NavBar
+  },
+  setup() {
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/token/",
+      type:"post",
+      data:{
+        username:'wjw',
+        password:'pwjw',
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      } 
+    });
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/info/",
+      type:"get",
+      headers:{
+        Authorization:"Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwYWRkYzJmOTlkMjE0OGZkOGEwNDIwY2RiMWE0MTNkOCIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY3MzAxMDcxNCwiZXhwIjoxNjc0MjIwMzE0fQ.FOfrIc7TgKD0LNmuz7HUPWLP8I0WbwcSaGAxtTYWkLU",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      } 
+    });
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/register/",
+      type:"post",
+      data:{
+        username:'yzc2',
+        password:'123',
+        confirmedPassword:'123',
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      } 
+    });
   }
 }
 </script>
