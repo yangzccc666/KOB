f79c299f95ab69d1ce167debdadb39856cb360c1
yangzccc666 <yangzccc@gmail.com>
2023-01-07 17:51:45 +0800

    配置mysql与注册登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,69 +1,41 @@
 <template>
-  <NavBar></NavBar>
-  <router-view/>
+  <NavBar />
+  <router-view></router-view>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import $ from 'jquery'
 
 export default {
-  components:{
+  components: {
     NavBar
   },
   setup() {
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/token/",
-      type:"post",
-      data:{
-        username:'wjw',
-        password:'pwjw',
-      },
-      success(resp) {
-        console.log(resp);
-      },
-      error(resp) {
-        console.log(resp);
-      } 
-    });
 
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/info/",
-      type:"get",
-      headers:{
-        Authorization:"Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwYWRkYzJmOTlkMjE0OGZkOGEwNDIwY2RiMWE0MTNkOCIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY3MzAxMDcxNCwiZXhwIjoxNjc0MjIwMzE0fQ.FOfrIc7TgKD0LNmuz7HUPWLP8I0WbwcSaGAxtTYWkLU",
-      },
-      success(resp) {
-        console.log(resp);
-      },
-      error(resp) {
-        console.log(resp);
-      } 
-    });
 
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/register/",
-      type:"post",
-      data:{
-        username:'yzc2',
-        password:'123',
-        confirmedPassword:'123',
-      },
-      success(resp) {
-        console.log(resp);
-      },
-      error(resp) {
-        console.log(resp);
-      } 
-    });
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "yxc2",
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
 
 <style>
-body{
+body {
   background-image: url("./assets/images/background.png");
   background-size: cover;
 }
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,6 +4,9 @@ import RecordIndexView from '../views/record/RecordIndexView'
 import RankListView from '../views/ranklist/RankListView'
 import UserIndexView from '../views/user/UserIndexView'
 import NotFoundView from '../views/error/NotFoundView'
+import UserAccountRegisterView from '@/views/user/account/UserAccountRegisterView'
+import UserAccountLoginView from '@/views/user/account/UserAccountLoginView'
+import store from '../store/index'
 
 
 const routes = [
@@ -11,31 +14,65 @@ const routes = [
     path:"/",
     name:"home",
     redirect:"/pk/", //重定向
+    meta: {
+      requsetAuth: true,
+    }
   },
   {
     path:"/pk/",
     name:"pk_index",
     component: PkIndexView,
+    meta: {
+      requsetAuth: true,
+    }
   },
   {
     path:"/record/",
     name:"record_index",
     component: RecordIndexView,
+    meta: {
+      requsetAuth: true,
+    }
   },
   {
     path:"/ranklist/",
     name:"ranklist_index",
     component: RankListView,
+    meta: {
+      requsetAuth: true,
+    }
   },
   {
     path:"/user/bot/",
     name:"user_bot_index",
     component: UserIndexView,
+    meta: {
+      requsetAuth: true,
+    }
+  },
+  {
+    path:"/user/account/login/",
+    name:"user_account_login",
+    component: UserAccountLoginView,
+    meta: {
+      requsetAuth: false,
+    }
+  },
+  {
+    path:"/user/account/register/",
+    name:"user_account_register",
+    component: UserAccountRegisterView,
+    meta: {
+      requsetAuth: false,
+    }
   },
   {
     path:"/404/",
     name:"404",
     component: NotFoundView,
+    meta: {
+      requsetAuth: false,
+    }
   },
   {
     path:"/:catchAll(.*)",
@@ -46,6 +83,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requsetAuth && !store.state.user.is_login) {
+    next({name:'user_account_login'});
+  } else {
+    next();
+  }
 })
 
 export default router
